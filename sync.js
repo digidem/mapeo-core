1b311aa27acd02ea6a16d58af2ba0a586f2fbf97
noffle <sww@eight45.net>
2019-02-13 14:21:13 -0800

    remove socket field

--- sync.js
+++ sync.js
@@ -232,8 +232,8 @@ function isGzipFile (filepath, cb) {
 
 function WifiTarget (connection, peer) {
   peer.type = 'wifi'
-  peer.socket = connection
   peer.swarmId = peer.swarmId || peer.id
+  // XXX: this is so that each connection has a unique id, even if it's from the same peer.
   peer.id = (!peer.id || peer.id.length!==12) ? randombytes(6).toString('hex') : peer.id
   return peer
 }
