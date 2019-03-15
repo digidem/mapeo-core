499621fef0eb94d42e3d39b3349bcfade2aa48f3
Karissa McKelvey <krmckelv@gmail.com>
2019-03-14 17:39:03 -0700

    push the fake outside of the feed cb

--- test/kappahealth.js
+++ test/kappahealth.js
@@ -3,12 +3,12 @@ var hyperhealth = require('hyperhealth')
 module.exports = function (core) {
   var healths = []
   healths.push(hyperhealth(core._logs._feeds.default))
+  healths.push(hyperhealth(core._logs._fake))
   core._logs.writer(function (err, writer) {
     if (err) throw err
     healths.push(hyperhealth(writer))
   })
   core._logs.on('feed', function (feed) {
-    healths.push(hyperhealth(core._logs._fake))
     healths.push(hyperhealth(feed))
   })
   function get () {
