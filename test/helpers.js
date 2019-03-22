66dbb0d50578f3766b87e04918d984a368b2ca56
noffle <sww@eight45.net>
2019-03-21 17:03:48 -0700

    test: use level@4

--- test/helpers.js
+++ test/helpers.js
@@ -2,7 +2,10 @@ var mkdirp = require('mkdirp')
 var path = require('path')
 var rimraf = require('rimraf')
 var randombytes = require('randombytes')
-var Osm = require('osm-p2p')
+var osmdb = require('kappa-osm')
+var kappa = require('kappa-core')
+var raf = require('random-access-file')
+var level = require('level')
 var blobstore = require('safe-fs-blob-store')
 var tmp = require('tmp')
 
@@ -14,10 +17,20 @@ module.exports = {
 
 function createApi (_, opts) {
   var dir = tmp.dirSync().name
+
   rimraf.sync(dir)
   mkdirp.sync(dir)
-  var osm = Osm(dir)
+
+  var osm = osmdb({
+    core: kappa(dir, {valueEncoding: 'json'}),
+    index: level(path.join(dir, 'index')),
+    storage: function (name, cb) {
+      process.nextTick(cb, null, raf(path.join(dir, 'storage', name)))
+    }
+  })
+
   var media = blobstore(path.join(dir, 'media'))
+
   return new Mapeo(osm, media, Object.assign({}, opts, {
     id: randombytes(8).toString('hex')
   }))
