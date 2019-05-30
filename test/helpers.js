0496888b8cd6a9f51399df876a8bbe623cf80169
noffle <sww@eight45.net>
2019-05-30 12:10:43 -0700

    test: more debug info on media creation

--- test/helpers.js
+++ test/helpers.js
@@ -70,7 +70,15 @@ function writeBigData (mapeo, n, cb) {
       tasks.push(task)
 
       if (tasks.length === n) {
-        run(tasks, 10, cb)
+        run(tasks, 10, function (err) {
+          mapeo.media.list(function (_, files) {
+            if (files.length !== n * 3) {
+              console.log('ERROR: not enough images were created! have:')
+              files.forEach(f => console.log(f))
+            }
+            cb(err)
+          })
+        })
       }
     })
   })
