0b88af6b8bb3ef7eea930c05a08b8ff8a042687f
Karissa McKelvey <krmckelv@gmail.com>
2019-05-30 17:04:57 -0700

    run all tests

--- test/sync.js
+++ test/sync.js
@@ -573,7 +573,7 @@ tape('sync: mobile <-> mobile photos', function (t) {
   })
 })
 
-tape.only('sync: with two peers available, sync with one only triggers events for one sync', function (t) {
+tape('sync: with two peers available, sync with one only triggers events for one sync', function (t) {
   var opts = {api1:{name: 'boop', deviceType:'desktop'}, api2:{name: 'beep', deviceType:'desktop'}}
   createApis(opts, function (api1, api2, close1) {
     var opts = {api1:{name: 'bork', deviceType:'mobile'}, api2:{name: 'baz', deviceType:'mobile'}}
